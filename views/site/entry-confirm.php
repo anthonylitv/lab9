5e7149118ed44dfa1e728c18ceb367ec7beba6e3
antonfront <toxalitvinenko@gmail.com>
2023-12-16 20:02:48 +0200

    first

---
+++ views/site/entry-confirm.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\helpers\Html;
+
+?>
+
+<p>Ви вказали наступну інформацію:</p>
+
+
+<ul>
+
+    <li><label>Ім’я</label>: <?= Html::encode($model->name) ?></li>
+
+    <li><label>Адреса електронної пошти</label>: <?= Html::encode($model->email) ?></li>
+
+</ul>
